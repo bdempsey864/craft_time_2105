87dd475503b1c0563c5823dfb6cc9a95e1ce5bce
Blake Dempsey <bdempsey864@gmail.com>
2021-06-08 13:13:41 -0600

    Add attributes

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Person do
   it 'exists' do
     expect(@person).to be_a(Person)
   end
+
+  it 'has attributes' do
+    expect(@person.name).to eq('Hector')
+    expect(@person.interests).to eq(['sewing', 'millinery', 'drawing'])
+    expect(@person.supplies).to eq({})
+  end
 end
